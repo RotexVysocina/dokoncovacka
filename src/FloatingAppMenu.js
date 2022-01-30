5b82205dc1670e369f8382012809199ae69a1af5
JakubAndrysek <jakub.andrysek03@gmail.com>
2022-01-30 14:16:57 +0100

    Fix manifest loading

--- src/FloatingAppMenu.js
+++ src/FloatingAppMenu.js
@@ -24,9 +24,6 @@ function FloatingAppMenu() {
         spacing={8}
         isOpen={isOpen}
       >
-        {
-
-        }
         <MainButton
           iconResting={<List color="royalblue" size={menuSize.bigIcon} />}
           iconActive={<List color="royalblue" size={menuSize.bigIcon} />}
@@ -64,66 +61,3 @@ function FloatingAppMenu() {
 }
 
 export default FloatingAppMenu;
-
-// class App extends React.Component {
-//   menuSize;
-//
-//   constructor(props) {
-//     super(props);
-//     this.this.menuSize = {
-//       bigMenu: 80,
-//       bigIcon: 45,
-//       smallMenu: 55,
-//       smallIcon: 35,
-//     }
-//
-//     this.state = {
-//       isOpen: false,
-//     }
-//   }
-//
-//   render() {
-//     return(
-//       <div className="menuButton">
-//         <FloatingMenu
-//           slideSpeed={500}
-//           direction={Directions.Up}
-//           spacing={8}
-//           isOpen={this.isOpen}
-//           class
-//         >
-//           <MainButton
-//             iconResting={<List color="royalblue" size={this.menuSize.bigIcon} />}
-//             iconActive={<List color="royalblue" size={this.menuSize.bigIcon} />}
-//             background="white"
-//             // onClick={() => setIsOpen((prev) => !prev)}
-//             onClick={() => this.setState(function (state) {
-//               return {
-//                 isOpen: !state.constructor,
-//               };
-//             })}
-//             size={this.menuSize.bigMenu}
-//           />
-//           <ChildButton
-//             icon={<HouseFill color="royalblue" size={this.menuSize.smallIcon} />}
-//             background="white"
-//             size={this.menuSize.smallMenu}
-//             onClick={() => {navigate("/"); setIsOpen(false);}}
-//           />
-//           <ChildButton
-//             icon={<Table color="royalblue" size={this.menuSize.smallIcon} />}
-//             background="white"
-//             size={this.menuSize.smallMenu}
-//             onClick={() => {navigate("/data"); setIsOpen(false);}}
-//           />
-//           <ChildButton
-//             icon={<PieChartFill color="royalblue" size={this.menuSize.smallIcon} />}
-//             background="white"
-//             size={this.menuSize.smallMenu}
-//             onClick={() => {navigate("/table"); setIsOpen(false);}}
-//           />
-//         </FloatingMenu>
-//       </div>
-//     )
-//   }
-// }
